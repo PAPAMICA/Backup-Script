71d8c6b4e5ac82214e6a5f61a974edb41473a021
PAPAMICA <asselinemickael@gmail.com>
2021-06-18 21:26:39 +0200

    fix calcul

--- backup-script.sh
+++ backup-script.sh
@@ -52,8 +52,7 @@ else
     DRY2=""
     DRY_RUN="no"
 fi
-FOLDER_TOTAL_SIZE=0
-FREE_SPACE_H=$(df $WORKFOLDER | awk 'FNR==2{print $4}')
+FREE_SPACE_H=$(df -h $WORKFOLDER | awk 'FNR==2{print $4}')
 FREE_SPACE=$(df $WORKFOLDER | awk 'FNR==2{print $4}')
 
 # Installation of requirements
@@ -106,7 +105,7 @@ function Backup-Folders {
         echo ""
         FOLDER_SIZE_H=$(du -hs /var/lib/docker $ARG_EXCLUDE_FOLDER $ARG_EXCLUDE_EXTENSIONS | awk '{print $1}')
         FOLDER_SIZE=$(du -s /var/lib/docker $ARG_EXCLUDE_FOLDER $ARG_EXCLUDE_EXTENSIONS | awk '{print $1}')
-        FOLDER_TOTAL_SIZE= $(echo $($FOLDER_TOTAL_SIZE + $FOLDER_SIZE) | bc)
+        FOLDER_TOTAL_SIZE= $(echo $FOLDER_TOTAL_SIZE + $FOLDER_SIZE | bc)
         echo "[$(date +%Y-%m-%d_%H:%M:%S)]   BackupScript   🌀   Backup of Docker folders ($FOLDER_SIZE_H) started."
         if [[ $DRY_RUN == "yes" ]]; then
                 $DRY "Backup $FOLDER (with $ARG_EXCLUDE_FOLDER and $ARG_EXCLUDE_FOLDER) to $BACKUPFOLDER/docker-$DATE.tar.gz" $DRY2
@@ -162,6 +161,7 @@ function Backup-Database {
          if [[ DRY_RUN == "no" ]]; then
             DB_TOTAL_SIZE_H=$(du -hs $BACKUPFOLDER/databases/ | awk '{print $1}')
             DB_TOTAL_SIZE=$(du -s $BACKUPFOLDER/databases/ | awk '{print $1}')
+        fi
 
 
     done
@@ -223,3 +223,4 @@ if [[ $DRY_RUN == "yes" ]]; then
     Dry-informations
 else
     Run-informations
+fi
